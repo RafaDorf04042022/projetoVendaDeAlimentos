3d7ed184c36b1e54ed478d402b2bd1b51ac00ffe
YtBelll <ytbell@outlook.com>
2023-06-12 10:58:31 -0300

    Tela cadastro em javaFX
    a tela cadastro e uma parte auto gerada do controle da tela cadastro com um comentario facilitando o uso dos nomes

---
+++ src/controller/TelaCadastroController.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Heber
+ */
+public class TelaCadastroController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     * 
+     * Nomes //IMPORTANTE
+     * Botôes:
+     *  Cadastro: telaCadastroButtonCadastrar
+     *      Ação: telaCadastroButtonCadastrarAction
+     *  
+     *  Limpar: telaCadastroButtonLimpar
+     *      Ação: telaCadastroButtonLimparAction
+     *  
+     *  Sair: telaCadastroButtonSair
+     *      Ação: telaCadastroButtonSairAction
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
